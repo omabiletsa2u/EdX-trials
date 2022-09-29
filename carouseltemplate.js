2ee86f7cdfbed39688d7267fc4184c6b14b8f63b
Ofentse Mabiletsa <omabiletsa@2u.com>
2022-09-29 09:32:48 +0200

    js test first commit

---
+++ carouseltemplate.js
@@ -0,0 +1,16 @@
+// <![CDATA[
+    console.log("This works");
+var slideIndex = 1;
+showDivs(slideIndex);
+function plusDivs(n) {showDivs(slideIndex += n);}
+function showDivs(n) {
+var i;
+var x = document.querySelectorAll(".slideShow");
+if (n > x.length) {slideIndex = 1};
+if (n < 1) {slideIndex = x.length};
+for (i = 0; i < x.length; i++) {
+x[i].style.display = "none";
+};
+x[slideIndex-1].style.display = "block";
+}
+// ]]>
